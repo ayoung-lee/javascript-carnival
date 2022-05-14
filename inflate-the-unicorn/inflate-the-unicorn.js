cdf192a2fd317918a7d4dd45947ba43e910f7355
ayoung-lee <aylee0729@gmail.com>
2022-05-14 14:24:25 +1200

    update inflate-the-unicorn work in progress

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,25 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+let unicorns = document.getElementsByClassName('inflate-an-image')
+
+for (let u = 0; u < unicorns.length; u++) {
+  unicorns[u].onclick = unicornClicked
+}
+
+function unicornClicked(e) {
+  let unicorns = e.target
+
+  if (unicorns.src.match('images/unicorn-0.png')) {
+    unicorns.src = 'images/unicorn-1.png'
+  } else if (unicorns.src.match('images/unicorn-1.png')) {
+    unicorns.src = 'images/unicorn-2.png'
+  } else if (unicorns.src.match('images/unicorn-2.png')) {
+    unicorns.src = 'images/unicorn-3.png'
+  } else if (unicorns.src.match('images/unicorn-3.png')) {
+    alert('Unicorn Says Thank You !')
+  }
+}
